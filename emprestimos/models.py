fa6c8ee68170397a46ca47943f5295db48926fde
marcusviniciusfelipea <marcusviniciusfelipeavelino@gmail.com>
2026-09-21 14:20:04 +0000

    Finaliza modelos e administracao do banco

--- emprestimos/models.py
+++ emprestimos/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from exemplares.models import Exemplar
+from usuarios.models import Usuario
+from prazo.models import Prazo
 
-# Create your models here.
+
+class Emprestimo(models.Model):
+    id = models.AutoField(primary_key=True)
+    data = models.DateField()
+    previsao_devolucao = models.DateField()
+
+    exemplar = models.ForeignKey(
+        Exemplar,
+        on_delete=models.CASCADE,
+        db_column='ID_exemplar'
+    )
+
+    usuario = models.ForeignKey(
+        Usuario,
+        on_delete=models.CASCADE,
+        db_column='ID_usuario'
+    )
+
+    prazo = models.ForeignKey(
+        Prazo,
+        on_delete=models.CASCADE,
+        db_column='ID_prazo'
+    )
+
+    def __str__(self):
+        return f'Empréstimo {self.id}'
